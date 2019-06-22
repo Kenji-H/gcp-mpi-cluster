0ab2478e197389facd95fe128c4bf3d332dd009b
Kenji-H <kenji1226h@gmail.com>
2019-06-22 20:37:18 +0900

    use local name for MPI hosts file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "instance_ips" {
   value = "${google_compute_instance.mpi_nodes.*.network_interface.0.access_config.0.nat_ip}"
 }
+
+output "instance_names" {
+  value = "${google_compute_instance.mpi_nodes.*.name}"
+}
